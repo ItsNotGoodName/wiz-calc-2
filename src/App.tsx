c91a0ddcc8789b4cb8ea767c06abad087a9276eb
ItsNotGoodName <gurnaindeol@gmail.com>
2020-10-17 15:55:25 -0700

    Add Spells

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import { Box, Flex } from "@chakra-ui/core";
-import React from "react";
-import { AddCard } from "./components/AddCard";
+import { Box, Flex, IconButton } from "@chakra-ui/core";
+import React, { useState } from "react";
 import { MainCard } from "./components/MainCard";
 import { SpellCard } from "./components/SpellCard";
 import { useCharacter } from "./components/UseCharacter";
+import { v4 } from "uuid";
 
 const App: React.FC = () => {
   const [character, setCharacter] = useCharacter();
+
+  const [spells, setSpells] = useState<string[]>([]);
+
   return (
     <Box maxW="900px" mt="5px" mx="auto">
       <Flex float="left" minW="250px" w="250px" mr="10px">
         <MainCard character={character} dispatch={setCharacter} />
       </Flex>
       <Flex wrap="wrap">
-        <Flex w="250px">
-          <SpellCard character={character} />
-        </Flex>
-        <Flex w="250px">
-          <AddCard />
+        {spells.map((value, index) => (
+          <Flex key={index} mb="auto" pb="10px" w="250px" mr="10px">
+            <SpellCard character={character} />
+          </Flex>
+        ))}
+        <Flex minH="250px" w="250px" mr="10px">
+          <IconButton
+            variantColor="blue"
+            m="auto"
+            aria-label="Add Spellcard"
+            icon="add"
+            onClick={() => {
+              const newSpells = [...spells];
+              newSpells.push(v4());
+              setSpells(newSpells);
+            }}
+          />
         </Flex>
       </Flex>
     </Box>
--- src/components/AddCard.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@chakra-ui/core";
-import React from "react";
-
-type AddCardProps = {};
-
-export const AddCard: React.FC<AddCardProps> = () => {
-  return <Button m="auto">Add Spellcard</Button>;
-};
