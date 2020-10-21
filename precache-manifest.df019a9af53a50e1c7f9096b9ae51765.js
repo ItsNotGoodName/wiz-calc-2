f75458e173b7f41b3757b5e9ce62527afa41ca87
ItsNotGoodName <gurnaindeol@gmail.com>
2020-10-20 19:48:51 -0700

    Updates

---
+++ precache-manifest.df019a9af53a50e1c7f9096b9ae51765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab59493f492eb94a1bacd3d0d32ef13e",
+    "url": "/wiz-calc-2/index.html"
+  },
+  {
+    "revision": "a117a4f103c82d83407f",
+    "url": "/wiz-calc-2/static/js/2.3072c4d8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/wiz-calc-2/static/js/2.3072c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e734672c26615de21b4",
+    "url": "/wiz-calc-2/static/js/main.a250a09b.chunk.js"
+  },
+  {
+    "revision": "466391f28bc50f36e8c8",
+    "url": "/wiz-calc-2/static/js/runtime-main.283345f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wiz-calc-2/precache-manifest.1c45c7e841f6428274a541ea6e0f7b45.js"
+  "/wiz-calc-2/precache-manifest.df019a9af53a50e1c7f9096b9ae51765.js"
 );
 
 self.addEventListener('message', (event) => {
