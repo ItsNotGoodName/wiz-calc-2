b67be321ffc0725e2c5f2f777834a3680097f3ce
ItsNotGoodName <gurnaindeol@gmail.com>
2020-10-20 17:19:44 -0700

    decimal

--- src/components/LabelNumber.tsx
+++ src/components/LabelNumber.tsx
@@ -1,11 +1,17 @@
-import { Box, Input, InputProps, Text } from "@chakra-ui/core";
+import {
+  Box,
+  Input,
+  InputProps,
+  NumberIncrementStepper,
+  Text,
+} from "@chakra-ui/core";
 import React from "react";
 
 export const LabelNumber: React.FC<InputProps> = ({ children, ...props }) => {
   return (
     <Box>
       <Text>{children}</Text>
-      <Input {...props} />
+      <Input type="number" {...props}></Input>
     </Box>
   );
 };
